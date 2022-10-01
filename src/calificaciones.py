e221b4000f333095ab21612af4d3bc429a1e7cbd
josemaMG <josema.mg20@gmail.com>
2022-10-01 21:15:03 +0200

    ejercicio 1, apartado a

---
+++ src/calificaciones.py
@@ -0,0 +1,3 @@
+def nota(aciertos , errores , numero_respuestas):
+    return ((aciertos*10)/numero_respuestas) - ((errores*10)/(50 - numero_respuestas))
+
---
+++ src/calificaciones_test.py
@@ -0,0 +1,4 @@
+from calificaciones import nota
+# Primero el número de aciertos, después el número de errores y por último el número de 
+# respuestas correctas
+print(nota(23 , 3 , 27))
