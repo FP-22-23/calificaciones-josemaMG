5594d20a77e53f6ee47b706ecf6c357641c8ce5b
josemaMG <114393829+josemaMG@users.noreply.github.com>
2022-10-02 17:30:05 +0200

    ejercicio 1 , completado

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -1,3 +1,11 @@
 def nota(aciertos , errores , numero_respuestas):
     return ((aciertos*10)/numero_respuestas) - ((errores*10)/(50 - numero_respuestas))
-
+def nota2():
+    print("Escriba el Nº de aciertos")
+    a = int(input()) 
+    print("Escriba el Nº de errores")
+    b = int(input()) 
+    print("Escriba el Nº de aciertos totales")
+    c = int(input())
+    print("===Cálculo de nota===")
+    print("Si el Nº de aciertos es" , a ,"el Nº de errores es", b ,"y el Nº de respuestas correctas era", c ,", su nota final es", ((a*10)/c)-(b*10)/(50-c))
--- src/calificaciones_test.py
+++ src/calificaciones_test.py
@@ -1,4 +1,5 @@
-from calificaciones import nota
+from calificaciones import nota , nota2
 # Primero el número de aciertos, después el número de errores y por último el número de 
 # respuestas correctas
 print(nota(23 , 3 , 27))
+print(nota2())
