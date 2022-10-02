bfb0c6726cf75e0883e68897a800192ffdc0c225
josemaMG <114393829+josemaMG@users.noreply.github.com>
2022-10-02 19:30:28 +0200

    Ejercicio 2 , apartado a

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -1,5 +1,6 @@
 def nota(aciertos , errores , numero_respuestas):
     return ((aciertos*10)/numero_respuestas) - ((errores*10)/(50 - numero_respuestas))
+
 def nota2():
     print("Escriba el Nº de aciertos")
     a = int(input()) 
@@ -9,3 +10,17 @@ def nota2():
     c = int(input())
     print("===Cálculo de nota===")
     print("Si el Nº de aciertos es" , a ,"el Nº de errores es", b ,"y el Nº de respuestas correctas era", c ,", su nota final es", ((a*10)/c)-(b*10)/(50-c))
+
+def calcula_nota_cuatrimestre(parcial , proyecto):
+    result = 0    
+    if proyecto < 5:
+        return result + 3  
+    else:
+        print("Indique la nota del primer cuestionario en el cuatrimestre")
+        C1 = float(input())
+        print("Indique la nota del segundo cuestionario en el cuatrimestre")
+        C2 = float(input())
+        print("Indique la nota del tercer cuestionario en el cuatrimestre")
+        C3 = float(input())
+        cuestionarios = (C1 , C2 , C3)
+        return result + sum(cuestionarios) * 0.1 + parcial * 0.6 + proyecto * 0.1
--- src/calificaciones_test.py
+++ src/calificaciones_test.py
@@ -1,5 +1,11 @@
-from calificaciones import nota , nota2
+from calificaciones import nota , nota2 , calcula_nota_cuatrimestre 
 # Primero el número de aciertos, después el número de errores y por último el número de 
-# respuestas correctas
+# respuestas correctas.
 print(nota(23 , 3 , 27))
+
+# Otra forma de saber la nota que se pedira los valores en el terminal de abajo.
 print(nota2())
+
+# El primer valor es la nota del práctico y el otro es la nota del proyecto
+# Dentro del proceso, si proyecto es mayor que 5, te pedira tus notas de los cuestionarios
+print(calcula_nota_cuatrimestre(5 , 6))
