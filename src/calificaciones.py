3130a93b22ccb30fdd03440bd5341a7e6121605f
josemaMG <114393829+josemaMG@users.noreply.github.com>
2022-10-10 23:39:31 +0200

    Calificaciones al completo

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -1,26 +1,61 @@
 def nota(aciertos , errores , numero_respuestas):
     return ((aciertos*10)/numero_respuestas) - ((errores*10)/(50 - numero_respuestas))
 
+def darnumero():
+    int(input())
+
+def Dame_cuestionarios(j):
+    cuestionarios= []
+    for n in range(j):
+        cuestionarios.append(float(input(f"Dame la nota del cuestionario {n+1}:")))
+    return cuestionarios
+
+def notas_proyecto(h):
+    proyecto=[]
+    for y in range(h):
+        proyecto.append(float(input(f"Dame la nota del proyecto {y + 1}:")))
+    return proyecto
+
+def notas_practica(g):
+    practica=[]
+    for x in range(g):
+        practica.append(float(input(f"Dame la nota de la práctica {x + 1}:")))
+    return practica
+
+def darnumero():
+    int(input())
+
 def nota2():
     print("Escriba el Nº de aciertos")
-    a = int(input()) 
+    a = darnumero() 
     print("Escriba el Nº de errores")
-    b = int(input()) 
+    b = darnumero() 
     print("Escriba el Nº de aciertos totales")
-    c = int(input())
+    c = darnumero()
     print("===Cálculo de nota===")
     print("Si el Nº de aciertos es" , a ,"el Nº de errores es", b ,"y el Nº de respuestas correctas era", c ,", su nota final es", ((a*10)/c)-(b*10)/(50-c))
 
 def calcula_nota_cuatrimestre(parcial , proyecto):
     result = 0    
     if proyecto < 5:
-        return result + 3  
+        return print("tu nota del cuatrimestre es:" ,result + 3)  
+        
     else:
-        print("Indique la nota del primer cuestionario en el cuatrimestre")
-        C1 = float(input())
-        print("Indique la nota del segundo cuestionario en el cuatrimestre")
-        C2 = float(input())
+        C1 = float(input("Indique la nota del primer cuestionario en el cuatrimestre"))
+        C2 = float(input("Indique la nota del segundo cuestionario en el cuatrimestre"))
         print("Indique la nota del tercer cuestionario en el cuatrimestre")
         C3 = float(input())
         cuestionarios = (C1 , C2 , C3)
-        return result + sum(cuestionarios) * 0.1 + parcial * 0.6 + proyecto * 0.1
+        return print("tu nota del cuatrimestre es:" , result + sum(cuestionarios) * 0.1 + parcial * 0.6 + proyecto * 0.1)
+
+def calcula_nota_evaluacion_continua():
+    result = 0
+    cuestionarios = Dame_cuestionarios(6)
+    proyecto= notas_proyecto(2)
+    practica= notas_practica(2)
+    if cuestionarios[0] + cuestionarios[1] + cuestionarios[2] < 12 or cuestionarios[3] + cuestionarios[4] + cuestionarios[5] < 12:
+        print("Tu nota de evaluación continua es 4")
+    else:
+        return print("tu nota es:", (result + sum(cuestionarios)*0.05 + sum(proyecto) * 0.05 + sum(practica) * 0.3)) 
+        
+
--- src/calificaciones_test.py
+++ src/calificaciones_test.py
@@ -1,4 +1,4 @@
-from calificaciones import nota , nota2 , calcula_nota_cuatrimestre 
+from calificaciones import nota , nota2 , calcula_nota_cuatrimestre , calcula_nota_evaluacion_continua
 # Primero el número de aciertos, después el número de errores y por último el número de 
 # respuestas correctas.
 print(nota(23 , 3 , 27))
@@ -8,4 +8,5 @@ print(nota2())
 
 # El primer valor es la nota del práctico y el otro es la nota del proyecto
 # Dentro del proceso, si proyecto es mayor que 5, te pedira tus notas de los cuestionarios
-print(calcula_nota_cuatrimestre(5 , 6))
+print(calcula_nota_cuatrimestre(5 , 6)) 
+print(calcula_nota_evaluacion_continua())
